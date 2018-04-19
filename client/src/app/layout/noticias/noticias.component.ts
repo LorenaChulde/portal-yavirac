8a5aa28578c941fabc58e3f20f1800e81ee69cee
alejodaniel <adf.coronel@yavirac.edu.ec>
2018-04-19 10:32:39 -0500

    Signed-off-by: alejodaniel <adf.coronel@yavirac.edu.ec>

--- client/src/app/layout/noticias/noticias.component.ts
+++ client/src/app/layout/noticias/noticias.component.ts
@@ -36,7 +36,7 @@ export class NoticiasComponent implements OnInit {
 
     getNoticia(): void {
         this.busy = this.dataService
-            .getNoticia(2)
+            .getNoticia(1)
             .then(entidadesRecuperadas => {
                 this.noticia = entidadesRecuperadas;
                 console.log(this.noticia.titulo);
