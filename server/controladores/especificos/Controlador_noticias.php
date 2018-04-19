c58cd661cdeacf371c852829681a587a8eda5338
erickerraez <edg.erraez@yavirac.edu.ec>
2018-04-19 09:15:08 -0500

    Noticias

--- server/controladores/especificos/Controlador_noticias.php
+++ server/controladores/especificos/Controlador_noticias.php
@@ -4,7 +4,7 @@ class Controlador_noticias extends Controlador_Base
 {
    function leer_noticia($args)
    { 
-        $sql = "SELECT descripcion, titulo, fecha, idPagina From Noticia where id = ?;";
+        $sql = "SELECT descripcion, titulo, fecha  From Noticia where id = ?;";
         $parametros = array($args['idNoticia']);
         
         $respuesta = $this->conexion->ejecutarConsulta($sql,$parametros);
